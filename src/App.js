652f74ee760d09366ee54c4b832caa39e2ccf740
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-19 17:18:35 +0530

    created newsocean using class components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./component/Navbar";
+import NewsCategory from "./component/NewsCategory";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  apikey = process.env.REACT_APP_NEWS_OCEAN;
+  c = "Prasanna";
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <NewsCategory apikey={this.apikey} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/articles.js
@@ -0,0 +1,157 @@
+let articles = [
+  {
+    source: {
+      id: "bbc-news",
+      name: "BBC News",
+    },
+    author: null,
+    title:
+      "Brighton & Hove Albion 3-1 Southampton: Seagulls secure European football with win",
+    description:
+      "Evan Ferguson scores twice in the first half as Brighton beat Southampton in the Premier League to secure European football for the first time in their history.",
+    url: "https://www.bbc.co.uk/sport/football/65586962",
+    urlToImage:
+      "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/144EB/production/_129797138_gettyimages-1492014323.jpg",
+    publishedAt: "2023-05-21T15:02:19Z",
+    content:
+      "Evan Ferguson took his tally to 10 goals for the season after scoring twice against Southampton\r\nEvan Ferguson scored twice in the first half as Brighton beat Southampton in the Premier League to sec… [+4895 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title: "Ibrahimovic retires from football - Reuters.com",
+    description: "Ibrahimovic retires from football  Reuters.com",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiTmh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvaWJyYWhpbW92aWMtcmV0aXJlcy1mb290YmFsbC0yMDIzLTA2LTA0L9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-06-04T21:36:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "The Guardian",
+    },
+    author: "John Brewin",
+    title:
+      "Football Daily | An eagerly rubbernecking football public gears up for Doom Sunday",
+    description:
+      "Sign up now! Sign up now! Sign up now? Sign up now!Then there were three. The Premier League will have its Super Sunday filled with AS IT STANDS league tables, tearful fans in ill-considered fancy dress and perhaps even a repeat of last year’s fake-news hoax,…",
+    url: "https://www.theguardian.com/football/2023/may/23/football-daily-email-leicester-everton-leeds",
+    urlToImage:
+      "https://i.guim.co.uk/img/media/bc81626a26d1ff6da8271bd34f149757b2d2d71b/0_396_6000_3600/master/6000.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctZGVmYXVsdC5wbmc&enable=upscale&s=fdf7ed8a8c343e7e78a210a80a87caa3",
+    publishedAt: "2023-05-23T14:43:42Z",
+    content:
+      "SAD (SMITH, ALLARDYCE, DYCHE)\r\nThen there were three. The Premier League will have its Super Sunday filled with AS IT STANDS league tables, tearful fans in ill-considered fancy dress and perhaps even… [+4299 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title: "The week in Asian football - Reuters",
+    description: "The week in Asian football  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiRWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvd2Vlay1hc2lhbi1mb290YmFsbC0yMDIzLTA1LTIyL9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-05-22T03:14:52Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title: "The week in Asian football - Reuters",
+    description: "The week in Asian football  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiRWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvd2Vlay1hc2lhbi1mb290YmFsbC0yMDIzLTA2LTEyL9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-06-12T03:10:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: null,
+      name: "The Guardian",
+    },
+    author: "Rob Smyth",
+    title: "Argentina v Australia: international football friendly – live",
+    description:
+      "<ul><li>Updates from the Socceroos’ one-off match in China</li><li>Kick-off time in Beijing is 8pm/10pm AEST/1pm BST</li><li>Any thoughts? Get in touch with Rob on email</li></ul>There are four changes from the World Cup match against Argentina. Nathaniel Atk…",
+    url: "https://www.theguardian.com/football/live/2023/jun/15/argentina-vs-socceroos-australia-arg-aus-football-friendly-live-score-updates-kick-off-time-national-team-list-squad-soccer-china",
+    urlToImage:
+      "https://i.guim.co.uk/img/media/ae739c19549e652a3c00c83a07919549ed09aabd/0_490_8192_4918/master/8192.jpg?width=1200&height=630&quality=85&auto=format&fit=crop&overlay-align=bottom%2Cleft&overlay-width=100p&overlay-base64=L2ltZy9zdGF0aWMvb3ZlcmxheXMvdGctbGl2ZS5wbmc&enable=upscale&s=f9aeaa9f65c76eddd78ed9f6f3edcff0",
+    publishedAt: "2023-06-15T11:09:57Z",
+    content:
+      'The Socceroos are paying tribute to the pioneering Rale Rasic, who died last week aged 87.\r\n&lt;p lang=\\"en\\" dir=\\"ltr\\"&gt;A special touch added to our warm-up strip tonight &lt;br&gt;&lt;br&gt;A g… [+7389 chars]',
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title:
+      "Saudi champions Al Ittihad sign 'global football icon' Benzema - Reuters",
+    description:
+      "Saudi champions Al Ittihad sign 'global football icon' Benzema  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiZGh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvZnJhbmNlLXN0cmlrZXItYmVuemVtYS1qb2lucy1zYXVkaS1zaWRlLWFsLWl0dGloYWQtMjAyMy0wNi0wNi_SAQA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-06-06T19:27:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title: "Spain has racism problem, football federation chief says - Reuters",
+    description:
+      "Spain has racism problem, football federation chief says  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMiaWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvc3BhaW4taGFzLXJhY2lzbS1wcm9ibGVtLWZvb3RiYWxsLWZlZGVyYXRpb24tY2hpZWYtc2F5cy0yMDIzLTA1LTIyL9IBAA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-05-22T11:20:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title:
+      "Brighton secure European football with 3-1 win over Southampton - Reuters",
+    description:
+      "Brighton secure European football with 3-1 win over Southampton  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMicWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvYnJpZ2h0b24tc2VjdXJlLWV1cm9wZWFuLWZvb3RiYWxsLXdpdGgtMy0xLXdpbi1vdmVyLXNvdXRoYW1wdG9uLTIwMjMtMDUtMjEv0gEA?oc%3D5&gl=FR&hl=en-US&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-05-21T15:11:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+  {
+    source: {
+      id: "google-news",
+      name: "Google News",
+    },
+    author: null,
+    title:
+      "Monchi leaves Sevilla to become Aston Villa's President of Football ... - Reuters",
+    description:
+      "Monchi leaves Sevilla to become Aston Villa's President of Football ...  Reuters",
+    url: "https://consent.google.com/ml?continue=https://news.google.com/rss/articles/CBMieWh0dHBzOi8vd3d3LnJldXRlcnMuY29tL3Nwb3J0cy9zb2NjZXIvbW9uY2hpLWxlYXZlcy1zZXZpbGxhLWJlY29tZS1hc3Rvbi12aWxsYXMtcHJlc2lkZW50LWZvb3RiYWxsLW9wZXJhdGlvbnMtMjAyMy0wNi0xNi_SAQA?oc%3D5%26hl%3Den-CA%26gl%3DCA%26ceid%3DCA:en&gl=FR&hl=en-CA&cm=2&pc=n&src=1",
+    urlToImage: null,
+    publishedAt: "2023-06-16T19:57:00Z",
+    content:
+      "We use cookies and data to<ul><li>Deliver and maintain Google services</li><li>Track outages and protect against spam, fraud, and abuse</li><li>Measure audience engagement and site statistics to unde… [+1131 chars]",
+  },
+];
